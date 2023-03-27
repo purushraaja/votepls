25149576a816fce49abd44d7b2eaea7b95b5c1c8
purushraaja <r.chailo@enestech.net>
2023-03-27 14:47:00 +0300

    added some prikolchiki

--- votepls/settings.py
+++ votepls/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'votepls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'votepls',
+        'USER': 'votepls',
+        'PASSWORD': 'ceparol',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
